2552926f018ca53841740cf874eaf2e98602f8c0
Drakoxw <developer17@sinergiaonline.com>
2026-03-11 00:39:16 -0500

    feat: cors test 1

--- src/main/java/com/cloud_technological/aura_pos/config/WebConfig.java
+++ src/main/java/com/cloud_technological/aura_pos/config/WebConfig.java
@@ -11,10 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Aplica a todos los endpoints
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-                .allowedHeaders("*")
-                .allowCredentials(true) // Importante si manejas autenticación con cookies o tokens
+                .allowedHeaders("*") // Importante si manejas autenticación con cookies o tokens
                 .maxAge(maxAge);
     }
 }
