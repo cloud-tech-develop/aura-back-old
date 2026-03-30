7ba9a58e854eabaa3d034e74276d3dd5bbb7128d
Drakoxw <dragon12xw@gmail.com>
2026-03-29 23:55:06 -0500

    feat: agregar ciudad, ciudad_id y barrio a locales

---
+++ src/main/java/com/cloud_technological/aura_pos/repositories/locales/LocalJPARepository.java
@@ -0,0 +1,15 @@
+package com.cloud_technological.aura_pos.repositories.locales;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cloud_technological.aura_pos.entity.LocalEntity;
+
+@Repository
+public interface LocalJPARepository extends JpaRepository<LocalEntity, Long> {
+    boolean existsByEmpresaIdAndNombreAndActivoTrue(Long empresaId, String nombre);
+    boolean existsByEmpresaIdAndNombreAndActivoTrueAndIdNot(Long empresaId, String nombre, Long id);
+    Optional<LocalEntity> findByIdAndActivoTrue(Long id);
+}
